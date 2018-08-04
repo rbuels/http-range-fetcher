abd01752cd8be4c72ec876fd1b0bbd73e8629bea
Robert Buels <rbuels@gmail.com>
2018-08-04 15:01:05 -0700

    actually, remote http semantics from buffercache

--- src/bufferCache.js
+++ src/bufferCache.js
@@ -1,4 +1,3 @@
-const nodeUrl = require('url')
 const LRU = require('lru-cache')
 
 const CompositeBufferProxyHandler = require('./compositeBufferProxyHandler')
@@ -13,7 +12,7 @@ class BufferCache {
   /**
    *
    * @param {object} args
-   * @param {function} args.fetch async function with signature (url, start, end) => Promise(Buffer)
+   * @param {function} args.fetch async function with signature (key, start, end) => Promise(Buffer)
    * @param {number} [args.size] size in bytes of cache to keep
    * @param {number} [args.chunkSize] size in bytes of cached chunks
    * @param {number} [args.aggregationTime] time in ms over which to pool requests before dispatching them
@@ -33,19 +32,7 @@ class BufferCache {
     this.chunkCache = LRU({ max: Math.floor(size / chunkSize) })
   }
 
-  _removeFragmentFromUrl(u) {
-    const parsed = nodeUrl.parse(u)
-    if (!parsed) throw new Error(`invalid url "${u}"`)
-    delete parsed.hash
-    const formatted = nodeUrl.format(parsed)
-    if (!formatted) throw new Error(`invalid url "${u}"`)
-    return formatted
-  }
-
-  async get(url, position, length) {
-    // remove any fragment from the URL
-    const urlWithoutFragment = this._removeFragmentFromUrl(url)
-
+  async get(key, position, length) {
     // calculate the list of chunks involved in this fetch
     const firstChunk = Math.floor(position / this.chunkSize)
     const lastChunk = Math.floor((position + length - 1) / this.chunkSize)
@@ -53,10 +40,7 @@ class BufferCache {
     // fetch them all as necessary
     const fetches = new Array(lastChunk - firstChunk + 1)
     for (let chunk = firstChunk; chunk <= lastChunk; chunk += 1) {
-      fetches[chunk - firstChunk] = this._getChunk(
-        urlWithoutFragment,
-        chunk,
-      ).then(data => ({
+      fetches[chunk - firstChunk] = this._getChunk(key, chunk).then(data => ({
         data,
         chunkNumber: chunk,
       }))
@@ -71,17 +55,17 @@ class BufferCache {
     )
   }
 
-  _getChunk(url, chunkNumber) {
-    const key = `${url} chunk ${chunkNumber}`
-    const cachedPromise = this.chunkCache.get(key)
+  _getChunk(key, chunkNumber) {
+    const chunkKey = `${key}/${chunkNumber}`
+    const cachedPromise = this.chunkCache.get(chunkKey)
     if (cachedPromise) return cachedPromise
 
     const freshPromise = this.aggregator.fetch(
-      url,
+      key,
       chunkNumber * this.chunkSize,
       (chunkNumber + 1) * this.chunkSize,
     )
-    this.chunkCache.set(key, freshPromise)
+    this.chunkCache.set(chunkKey, freshPromise)
     return freshPromise
   }
 }
