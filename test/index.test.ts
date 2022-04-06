a71e14d238ef2d860de26bc8dca9c73e03684ffe
Robert Buels <rbuels@gmail.com>
2022-04-06 14:45:32 -0700

    lint

--- test/index.test.ts
+++ test/index.test.ts
@@ -153,4 +153,3 @@ it('can fetch a whole file with no content-range', async () => {
   expect([...new Uint8Array(got3.buffer)]).toEqual(range(0, 20))
   expect(calls).toEqual([['foo', 0, 19]])
 })
-
